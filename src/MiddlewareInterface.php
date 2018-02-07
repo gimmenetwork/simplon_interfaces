0809a93d4a6dbb1ee412f928ec5b4ab23364d514
Tino Ehrich <tino@bigpun.me>
2018-02-07 18:56:10 +0100

    added MiddlewareInterface

---
+++ src/MiddlewareInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Simplon\Interfaces;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+interface MiddlewareInterface
+{
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param callable|null $next
+     *
+     * @return ResponseInterface
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, ?callable $next = null): ResponseInterface;
+}
